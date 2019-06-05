7ca53b7adc43e7e7404ffcf05c65511a6542cc16
Eduardo Alarcon <eduardo.alarconu@gmail.com>
2019-06-05 10:28:11 -0400

    Prueba Finalizada

--- features/step_definitions/prueba.js
+++ features/step_definitions/prueba.js
@@ -4,7 +4,6 @@ const {
     Then
 } = require('cucumber');
 const Selector = require('testcafe').Selector;
-const asd = null;
 
 Given('Navego a la pagina de google', async function () {
     await testController.navigateTo('https://google.com');
@@ -12,45 +11,36 @@ Given('Navego a la pagina de google', async function () {
 
 When('Ingreso en la busqueda tottus sucursales', async function () {
     await testController
-    asd = Selector('[name="q"]').with({
-            boundTestRun: testController
-        })
-        .typeText(asd, 'tottus sucursales')
+        .typeText(Selector('[name="q"]'), 'tottus sucursales')
         .pressKey("enter")
-        .expect(Selector('b').innerText).contains('nataniel')
 });
 
 When('Presiono la busqueda tottus nataniel', async function () {
     await testController
         .click(Selector('b').withText('nataniel'))
-        .expect(Selector('a').innerText).contains('Sitio web')
 });
 
 When('Presiono el link Sitio web', async function () {
     await testController
         .click(Selector('a').withText('Sitio web'))
-        .expect(Selector('#tottusLoadingPage').innerText).contains('tottus')
 });
 
 When('Navego a Recetas', async function () {
     await testController
         .navigateTo('http://www.tottusrecetas.cl/')
-        .expect(Selector('button').innerText).contains('ALTA')
 });
 
 When('Presiono Dificultad Alta', async function () {
     await testController
-        .click(pSelector('button').withText('ALTA'))
-        .expect(Selector('h3').innerText).contains('CORONA DE ROLLITOS DE CANELA')
+        .click(Selector('button').withText('ALTA'))
 });
 
 When('Presiono la receta CORONA DE ROLLITOS DE CANELA', async function () {
     await testController
-        .click(pSelector('h3').withText('CORONA DE ROLLITOS DE CANELA'))
-        .expect(Selector('#hero').find('p').innerText).contains('CORONA DE ROLLITOS DE CANELA')
+        .click(Selector('h3').withText('CORONA DE ROLLITOS DE CANELA'))
 });
 
 Then('La receta contiene los ingredientes Azúcar rubia y canela', async function () {
     await testController
-        .expect(Selector('#hero').find('p').innerText).contains("CORONA DE ROLLITOS DE CANELA", 'azúcar rubia', 'canela')
+        .expect(Selector('#hero').find('p').with({ boundTestRun: testController }).innerText).contains("CORONA DE ROLLITOS DE CANELA", 'Azúcar rubia', 'Canela')
 });
